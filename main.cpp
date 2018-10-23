f4eb39822534c4fcbc99a33043e17f02cd2722b2
Óscar <oxcabe@gmail.com>
2018-10-23 16:31:26 +0100

    Encapsular inicialización de mapagrafico

--- main.cpp
+++ main.cpp
@@ -6,16 +6,8 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    QGridLayout *l1 = new MapaGrafico(10,10,20,10);
+    QWidget *w = new MainWindow(new MapaGrafico());
 
-    for(int i = 0; i < 10; i++){
-        for(int j = 0; j < 10; j++){
-            QPushButton *pb1 = new CeldaGrafica(0);
-            l1 -> addWidget(pb1, i, j, 1, 1);
-        }
-    }
-
-    QWidget *w = new MainWindow(l1);
     w -> show();
 
     return a.exec();
--- mapagrafico.cpp
+++ mapagrafico.cpp
@@ -4,7 +4,12 @@ MapaGrafico::MapaGrafico(int x, int y, int pObst, int nPeatones):
 x_(x), y_(y), porcentajeObstaculos_(pObst), nPeatones_(nPeatones),
 QGridLayout()
 {
-
+    for(int i = 0; i < 10; i++) {
+        for(int j = 0; j < 10; j++) {
+            QPushButton *pb1 = new CeldaGrafica(0);
+            addWidget(pb1, i, j, 1, 1);
+        }
+    }
 }
 
 MapaGrafico::~MapaGrafico(){}
