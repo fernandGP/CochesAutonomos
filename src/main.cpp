eb9377a0c613a6f6b5d213c146d228bfcbed759b
Miriam Rodriguez <miirii2394@gmail.com>
2018-10-31 13:49:19 +0000

    Main actualizado

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,37 @@
 #include "../headers/mapa.h"
 #include "../headers/mainwindow.h"
 #include <QApplication>
+#include <unistd.h>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+    int m,n,option;
+    cout << "--Dimensiones mapa--" << endl;
+    cout << "Introduce largo: "; cin >> m; cout << endl;
+    cout << "Introduce ancho:"; cin >> n; cout << endl;
+    cout << "--Modo de Visualización--" << endl;
+    cout << "1. Modo Consola" << endl;
+    cout << "2. Modo Gráfico" << endl;
+    cin >> option;
 
-    QWidget *w = new MainWindow(new MapaGrafico());
+    if(option == 1){
+        int obs, pea;
+        cout << "¿Cuánto porcentaje de obstáculos quiere?" << endl;
+        cin >> obs; cout << endl;
+        cout << "¿Cuánto porcentaje de peatones quiere?" << endl;
+        cin >> pea;
 
-    w -> show();
+        Mapa map(m,n,obs,pea);
+        map.visualizar();
+        while(true){sleep(200);}
+    }
+    else if(option == 2){
+        QApplication a(argc, argv);
 
-    return a.exec();
+        QWidget *w = new MainWindow(new MapaGrafico());
+
+        w -> show();
+
+        return a.exec();
+    }
 }
